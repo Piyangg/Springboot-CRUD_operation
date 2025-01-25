a249d543db23bb6062141acdf8c16be7c5e2fc19
PiaPelaez <pelaez.243020@ortigas-cainta.sti.edu.ph>
2025-01-25 22:00:01 +0800

    Update student's data

--- src/main/java/com/CRUD/CrudOperation/controller/StudentController.java
+++ src/main/java/com/CRUD/CrudOperation/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.CRUD.CrudOperation.controller;
 import com.CRUD.CrudOperation.entity.Student;
 import com.CRUD.CrudOperation.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,7 +28,18 @@ public class StudentController {
     }
 
     @PostMapping ("/students/add")
+    @ResponseStatus(code = HttpStatus.CREATED)
     public void CreateStudent(@RequestBody Student student){
         repo.save(student);
     }
+
+    @PutMapping("/students/update/{id}")
+    public Student updateStudents (@PathVariable int id) {
+       Student student = repo.findById(id).get();
+       student.setName("Piyang");
+       student.setPercentage(98);
+       repo.save(student);
+       return student;
+    }
+
 }
